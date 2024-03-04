eaad3e0b0f498a8c3ea53847d2bf366b182ab138
Osman Yusuf AKKUŞ <Osman.Akkus@ogr.gidatarim.edu.tr>
2024-03-04 13:37:01 +0300

    Listing page (#4)
    * Add new components: Button, EmptyState, and Heading
    
    * Add link to logo and search icon
    
    * Add new listing page and update empty state handling
    
    * Refactor container component styles
    
    ---------
    
    Co-authored-by: SaffetAdak <saffetadak_@hotmail.com>

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -6,14 +6,7 @@ interface ContainerProps {
 }
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div
-      className="
-      max-w-[2520px]
-      max-auto
-      xl:px-20
-      md:px-10
-      sm:px-2
-      px-4">
+    <div className="max-w-[2520px] max-auto xl:px-20 md:px-10 sm:px-2 px-4">
       {children}
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={font.className}>
-            <Navbar />
-          {children}
+          <Navbar />
+          <div className="pb-20 pt-20">{children}</div>
         </body>
       </html>
     </ClerkProvider>
