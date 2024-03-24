25b1772ec64ffe6d81192712e24dc91deb88880f
Osman Yusuf Akkus <osmanyusufakkus@gmail.com>
2024-03-24 16:08:39 +0300

    Refactor getListings function to accept a text parameter and remove double quotes from the text

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,14 +1,14 @@
-import prisma from "@/app/libs/prismadb"
+import prisma from "@/app/libs/prismadb";
 
-export default async function getListings() {
-    try{
-        const listings = await prisma.property.findMany({
-            where: {
-                Konut_Sekli: "Daire"
-            }
-        })
-        return listings;
-    }catch(error:any){
-        console.error(error)
-    }
+export default async function getListings(text: any) {
+  try {
+    const cleanedText = text.replace(/"/g, ""); // Remove all double quotes from the text
+    console.log("Cleaned Text:", cleanedText);
+    const listings = await prisma.property.findMany({
+      where: cleanedText,
+    });
+    return listings;
+  } catch (error: any) {
+    console.error(error);
+  }
 }
