c89bc5dbc8023428fff984e4a67242d054eab7da
SaffetAdak <saffetadak_@hotmail.com>
2024-03-26 03:47:01 +0300

    Update typewriter strings in ParentComponent

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const ParentComponent = () => {
     <Container>
       <div className="h-[calc(100vh-240px)] flex justify-center items-center font-extrabold text-4xl text-cyan-500">
         <Typewriter options={{
-          strings: ['İstanbul, Kadıköy\'de kiralık daire arıyorum <span style="color: #737A85;"> en az  100m2 olmalı</span>', ' <span style="color: #737A85;">2+1 eve ihtiyacım var, en az  100m2 olmalı</span> ve aylık kira ücreti 7500₺\'den az olsun'],
+          strings: ['İstanbul, Kadıköy\'de kiralık daire arıyorum <span style="color: #737A85;"> en az  100m2 olmalı</span>', ' <span style="color: #737A85;">2+1 eve ihtiyacım var, en az  100m2 olmalı</span> ve aylık kirası 7500₺\'den az olsun'],
           autoStart: true,
           loop: true,
         }}/>
