159dcc51ab495a7d0c3dadc41465729d36f4a35b
SaffetAdak <saffetadak_@hotmail.com>
2024-03-26 03:46:42 +0300

    Add Typewriter component to ParentComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import Container from "./components/Container";
+import Typewriter from 'typewriter-effect';
 
 const ParentComponent = () => {
   return (
     <Container>
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
-        <div>Evler</div>
+      <div className="h-[calc(100vh-240px)] flex justify-center items-center font-extrabold text-4xl text-cyan-500">
+        <Typewriter options={{
+          strings: ['İstanbul, Kadıköy\'de kiralık daire arıyorum <span style="color: #737A85;"> en az  100m2 olmalı</span>', ' <span style="color: #737A85;">2+1 eve ihtiyacım var, en az  100m2 olmalı</span> ve aylık kira ücreti 7500₺\'den az olsun'],
+          autoStart: true,
+          loop: true,
+        }}/>
       </div>
     </Container>
   );
