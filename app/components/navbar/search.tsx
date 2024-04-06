a5d047156dc27d9ace132d9046708cea22f67b73
SaffetAdak <saffetadak_@hotmail.com>
2024-04-06 18:53:48 +0300

    Add shadow to search bar in navbar

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Search = () => {
   return (
     <Link href="/api/searching">
-      <div className="border-[2px] border-cyan-500 w-full md:w-auto py-2 rounded-full hover:bg-cyan-50/25 transition cursor-pointer">
+      <div className="border-[2px] border-cyan-500 w-full md:w-auto py-2 rounded-full hover:bg-cyan-50/25 transition cursor-pointer shadow-lg">
         <div className="flex flex-row items-center justify-between">
           <div className="m-2 ml-5 text-gray-700/70">Ara ve Keşfet...</div>
           <div className="text-sm font-semibold px-2 p-2 mr-2 bg-cyan-500 rounded-full text-white shadow-sm hover:shadow-md transition cursor-pointer ">
