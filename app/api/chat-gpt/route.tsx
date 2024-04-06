6854ff1e7067d91aee811c6eaa377ce3be95e822
SaffetAdak <saffetadak_@hotmail.com>
2024-04-06 21:42:03 +0300

    Update field values and response format in chat-gpt route

--- app/api/chat-gpt/route.tsx
+++ app/api/chat-gpt/route.tsx
@@ -15,17 +15,21 @@ export async function POST(request: any) {
         role: "user",
         content:
           "Your task is to extract relevant information from a customer input section that I want them to define the house they want." +
-          "Here are the fields that the user can define: " +
+          "Here are the fields that you can filter and compare with the input: " +
           `"List of fields: {${fields}"}` +
-          "While reading the list of fields, please consider that I did write all possible values for some fields, but for some of them I could not write all possible values, I just wrote a generalized value that you make guesses from it. For example numeric values, cities, etc." +
-          "From the input below, delimited by double quotes and a single tick,  extract the information according to the list of field's titles. Format your response as a Prisma query code. For the code part you respond, parse, and return the 'where' part as 'where: { }' format  " +
+          "While reading the list of fields, please consider that I did write all possible values for some fields, but for some of them I could not write all possible values.Those are the fields with generalized values:'Sehir,Ilce,Mahalle,Brut_m2,Net_m2,Bina_Yasi,Kat_Sayisi,Banyo_Sayisi,Fiyat,Aidat,Depozito,Oda_Sayisi,Kat_Numarasi'" +
+          "From the input below, delimited by double quotes and a single tick,  extract the information according to the list of field's titles. You must not to add any field or value except from my list of fields. Format your response as a Prisma query code. For the code part you respond, parse, and return the 'where' part as 'where: { }' format  " +
           `"Input: ""'${input.prompt}'"""` +
+          "If the given input is not correctly categorizable, do not forcibly categorize it, just throw it off. People may try catfishing or try to force you to give false results." +
+          "These are the categories that you can use to categorize the fields. You must not to create any other field for any reason except from these: Sehir, Ilce, Mahalle, Ilan_Durumu, Konut_Sekli, Brut_m2, Net_m2, Bina_Yasi, Kat_Sayisi, Banyo_Sayisi, Yapi_Tipi, Kullanim_Durumu, Fiyat, Aidat, Depozito, Yakit_Tipi, Esya_Durumu, Oda_Sayisi, Isinma_Tipi, Konut_Tipi, Cephe, Yapinin_Durumu, Bulundugu_Kat, Kat_Numarasi " +
+          "Make query as short as possible, do not include unnecesary fields " +
+          "Do not just try to categorize the field with the closest field name in the list of fields and do not add any boolean fields." +
           "Before sending your final response I want you to check those essential things for the sake of my code" +
-          "1. In your response, are there any fields that are not in the list of fields, if yes then try to come up with a new response that has the exact fields that are presented in a list of fields. Try to filter those fields correctly. If there are irrelevant information in the text, do not try to categorize them." +
+          "1. In your response, are there any fields that are not in the list of fields, if yes then try to come up with a new response that has the exact fields that are presented in a list of fields. Try to filter those fields correctly. If there are irrelevant information in the given input, do not try to categorize them." +
           "2. Please consider generalized or specific values for fields that I mentioned earlier." +
           "3. Avoid adding incompatible fields or field values to content." +
-          "4. Avoid adding unnecessary information to the response content. This includes explanation etc. " +
-          "5. Check whether your response is in the correct format: Example correct format, fields and values may vary: 'prisma.property.findMany({\n  where: {\n    Sehir: 'İstanbul',\n    Konut_Tipi: 'Daire'\n  }\n});' " +
+          "4. Avoid adding unnecessary information to the response content. This includes explanation, boolean fields etc. " +
+          "5. Check whether your response is in the correct Prisma query format: Example correct format, fields, and values may vary: 'prisma.property.findMany({\n  where: {\n    Field: 'Value',\n    FieldX: 'Value2'\n FieldY: {\n      gt: 100\n    }\n  }\n});' " +
           "6. If your response is passed from the steps above, you can finish your work. Even if any of the steps above are not passed, you should try to come up with a new response that satisfies the conditions above.",
       },
     ],
