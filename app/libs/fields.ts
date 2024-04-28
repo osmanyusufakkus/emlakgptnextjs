8e5c23f209b82a23c8b8bb85c02cc84f5e69c890
mehmet cem doğan <mehmetcemdogan@hotmail.com>
2024-04-28 13:53:46 +0300

    Update field values and improved chat-gpt prompt.

--- app/libs/fields.ts
+++ app/libs/fields.ts
@@ -2,25 +2,25 @@ export const fields = [
   "'Sehir':'Türkiyedeki bütün şehirler'," +
     "'Ilce':Türkiyedeki bütün ilçeler'," +
     "'Mahalle':'Türkiyedeki bütün mahalleler'," +
-    "'Ilan_Durumu': 'Kiralık','Satılık'," +
-    "'Konut_Sekli': 'Ara Kat Dubleks','Bahçe Dubleksi','Bahçe Katı','Çatı Dubleksi','Daire','Dubleks','İkiz Ev','Müstakil Ev','Ters Dubleks','Tripleks','Fourlex'," +
+    "'Ilan_Durumu': ['Kiralık','Satılık']" +
+    "'Konut_Sekli': ['Ara Kat Dubleks','Bahçe Dubleksi','Bahçe Katı','Çatı Dubleksi','Daire','Dubleks','İkiz Ev','Müstakil Ev','Ters Dubleks','Tripleks','Fourlex']" +
     "'Brut_m2':'140'," +
     "'Net_m2':'120'," +
     "'Bina_Yasi':'15'," +
     "'Kat_Sayisi':'1','2','3','4','5'," +
     "'Banyo_Sayisi':2," +
-    "'Yapi_Tipi': 'Ahşap','Betonarme','Çelik','Kütük','Prefabrik','Taş Bina','Yığma'," +
-    "'Kullanim_Durumu': 'Boş','Kiracılı','Mülk Sahibi'," +
+    "'Yapi_Tipi': ['Ahşap','Betonarme','Çelik','Kütük','Prefabrik','Taş Bina','Yığma']" +
+    "'Kullanim_Durumu': ['Boş','Kiracılı','Mülk Sahibi']" +
     "'Fiyat':'20000'" +
     "'Aidat':'1350'," +
     "'Depozito':'20500'," +
-    "'Yakit_Tipi': 'Doğalgaz','Kömür-Odun','Akaryakıt','Elektrik'," +
-    "'Esya_Durumu': 'Eşyalı', 'Eşyasız'," +
+    "'Yakit_Tipi': ['Doğalgaz','Kömür-Odun','Akaryakıt','Elektrik']" +
+    "'Esya_Durumu': ['Eşyalı', 'Eşyasız']" +
     "'Oda_Sayisi':4+1','2+1','1+1','3+1','1+0','6+1','5+1','4+2'," +
-    "'Isinma_Tipi': 'Güneş Enerjisi','Kat Kaloriferi' ,'Klima','Kombi','Merkezi','Merkezi (Pay Ölçer)','Soba','Jeotermal Isıtma','Yerden Isıtma','Doğalgaz Sobası','Fancoil Ünitesi','VRV','Isı Pompası'," +
-    "'Konut_Tipi': 'Daire', 'Villa','Müstakil Ev','Prefabrik','Bina','Residence','Yazlık','Köy Evi','Çiftlik Evi','Köşk','Yalı Dairesi','Yalı','Dağ Evi','Loft Daire','Kooperatif'," +
-    "'Cephe': 'Kuzey', 'Güney', 'Doğu', 'Batı'," +
-    "'Yapinin_Durumu': 'İkinci El', 'Sıfır', 'Yapım Aşamasında'," +
-    "'Bulundugu_Kat_Tipi': 'Yüksek Giriş','Ara Kat','En Üst Kat','Çatı Katı','Teras Katı','Bodrum ','Yarı Bodrum','Bodrum ve Zemin','Zemin','Bahçe Katı','Giriş Katı','Villa Katı','Asma Kat','Kot 1','Kot 2'" +
+    "'Isinma_Tipi': ['Güneş Enerjisi','Kat Kaloriferi' ,'Klima','Kombi','Merkezi','Merkezi (Pay Ölçer)','Soba','Jeotermal Isıtma','Yerden Isıtma','Doğalgaz Sobası','Fancoil Ünitesi','VRV','Isı Pompası']" +
+    "'Konut_Tipi': ['Daire', 'Villa','Müstakil Ev','Prefabrik','Bina','Residence','Yazlık','Köy Evi','Çiftlik Evi','Köşk','Yalı Dairesi','Yalı','Dağ Evi','Loft Daire','Kooperatif']" +
+    "'Cephe': ['Kuzey', 'Güney', 'Doğu', 'Batı']" +
+    "'Yapinin_Durumu': ['İkinci El', 'Sıfır', 'Yapım Aşamasında']" +
+    "'Bulundugu_Kat_Tipi': ['Yüksek Giriş','Ara Kat','En Üst Kat','Çatı Katı','Teras Katı','Bodrum ','Yarı Bodrum','Bodrum ve Zemin','Zemin','Bahçe Katı','Giriş Katı','Villa Katı','Asma Kat','Kot 1','Kot 2']" +
     "'Bulundugu_Kat_Numarasi':'0','1','2',",
 ];
